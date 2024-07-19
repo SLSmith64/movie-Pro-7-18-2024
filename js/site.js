5269cfe321a33f792f0ea0a3723224adc04a32c3
shawn <shawn@rsatechnologyservices.com>
2024-07-19 11:41:48 -0500

    Worked on favorite button section

--- js/site.js
+++ js/site.js
@@ -1,12 +1,118 @@
-function getValues(){
- alert("Hello App!!!");
+
+//Function Calls
+//displayPopularMovies();
+displayNowPlayingMovies();
+
+//Display Popular Movies Function
+async function displayPopularMovies(){
+    let movies = await getPopularMovies();
+    
+    displayMovies(movies);
 }
 
-//get values from the screen and display them
-function getValues2(){
-    let inputMessage = document.getElementById("message").value; 
-
-    let alertMessage = document.getElementById("alertMessage");
-
-    alertMessage.innerText = inputMessage;
+//Display Now Playing Movies Function
+async function displayNowPlayingMovies(){
+    let movies = await getNowPlayingMovies();
+    
+    displayMovies(movies);
 }
+
+function displayMovies(movies){
+    
+    //Get the Movie card template.
+    const movieCardTemplate = document.getElementById("movie-card-template");
+    const movieRow = document.getElementById("movie-row");
+    movieRow.innerHTML = "";
+
+    //Find the movie row element where all the movies go.
+    movies.forEach(movie => {
+        
+        let movieCard = document.importNode(movieCardTemplate.content, true);
+
+        //Modify the card for the current movie
+        let titleElement = movieCard.querySelector(`.card-body > .card-title`);
+        titleElement.textContent = movie.title;
+
+        let descriptionElement = movieCard.querySelector(`.card-text`);
+        let date = new Date(movie.release_date);
+        descriptionElement.textContent = date.toLocaleDateString(undefined, {month: `short`, day: `numeric`, year:`numeric`});
+        
+        let movieImageElement = movieCard.querySelector(".card-img-top");
+        let poster_path = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        if (movie.poster_path == null){
+            poster_path = "/img/poster.png";
+        }
+        movieImageElement.setAttribute('src',poster_path);
+
+        //Set the buttons correctly
+        //Find favorite button
+        let removeFavButton = movieCard.querySelector('[data-fav="true"]');
+        removeFavButton.setAttribute('data-movieId', movie.id);
+
+        //
+        let addFavButton = movieCard.querySelector('[data-fav="false"]');
+        addFavButton.setAttribute('data-movieId', movie.id);
+
+        if(isFavoriteMovie(movie.id)){
+            addFavButton.style.display = 'none';
+            removeFavButton.style.display = 'block';
+        } else {
+            addFavButton.style.display = 'block';
+            removeFavButton.style.display = 'none';
+        }
+
+        movieRow.appendChild(movieCard);
+    });
+}
+
+/* #region favorite movies */
+
+async function addFavoriteMovie(btn){
+    let movieId = btn.getAttribute('data-movieId');
+
+    let favorites = getFavoriteMovies();
+
+    let duplicateMovie = favorites.find(movie => movie.id == movieId);
+
+    if (duplicateMovie == undefined){
+        let newFavorite = await getMovie(movieId);
+
+        if (newFavorite != undefined){
+            favorites.push(newFavorite);
+            saveFavoriteMovies(favorites);
+        }
+    }                                            
+}
+
+function getFavoriteMovies(){
+    let favoriteMovies = localStorage.getItem("ss-favorite-movies");
+
+    if(favoriteMovies == null){
+        favoriteMovies = [];
+        saveFavoriteMovies(favoriteMovies);
+    }
+    else{
+        favoriteMovies =JSON.parse(favoriteMovies);
+    }
+
+    return favoriteMovies;
+}
+
+//Save favorite movies to Local Storage.
+function saveFavoriteMovies(movies){
+    let movieJSON = JSON.stringify(movies);
+    localStorage.setItem("np-favorite-movies", movieJSON);
+
+}
+
+function isFavoriteMovie(movieId){
+    let favoriteMovies = getFavoriteMovies();
+
+    if(!favoriteMovies){
+        return false;
+    }
+
+    return favoriteMovies.some(movie => movie.id == movieId);
+}
+
+/* #endregion favorite movies */
---
+++ js/tmdbAPI.js
@@ -0,0 +1,76 @@
+const apiKey = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTMzMWRhZTEyNWM0NTc4NjBmZDEzMzFhYTYzYWY4MiIsIm5iZiI6MTcyMTMzNTA4OC42MjQ5MTksInN1YiI6IjY2OTk2Yzc2ZDE4ZTk4YzgwNzhiZjA2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gibXy44W1l5PX76nG-w0PoR6TM94jmEuOMdps2aVq6c`;
+
+/**
+ * Fetches and returns a list of popular movies currently playing in the theaters.
+ * @returns {Object} A list of currently playing movies as a javascript object
+ * @throws {Error} If the network response is not ok.
+ */
+async function getPopularMovies(){
+    const popularMoviesURL = `https://api.themoviedb.org/3/movie/popular`;
+
+    try{
+        let response = await fetch(popularMoviesURL,{
+            headers:{
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        if(!response.ok) throw new Error("Network response was not ok.");
+
+        let popularMovies = await response.json();
+        return popularMovies.results;
+    } catch (error){
+        console.error(`Fetch Error:  ${error}`);
+        return [];
+    }
+}
+
+async function getNowPlayingMovies(){
+    const getNowPlayingMovies = `https://api.themoviedb.org/3/movie/now_playing`;
+
+    
+    try{
+        let response = await fetch(getNowPlayingMovies,{
+            headers:{
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        if(!response.ok) throw new Error("Network response was not ok.");
+
+        let nowPlayingMovies = await response.json();
+        return nowPlayingMovies.results;
+    } catch (error){
+        console.error(`Fetch Error:  ${error}`);
+        return [];
+    }
+
+
+}
+
+/**
+ * 
+ * @param {number} movieId 
+ * @returns return an movie object from TMDB API
+ */
+async function getMovie(movieId){
+    
+    const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}`;
+
+    
+    try{
+        let response = await fetch(movieUrl,{
+            headers:{
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        if(!response.ok) throw new Error("Network response was not ok.");
+
+        let movie = await response.json();
+        return movie;
+    } catch (error){
+        console.error(`Get Movies Fetch Error:  ${error}`);
+        return null;
+    }
+}
